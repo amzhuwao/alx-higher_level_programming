88455325f43cf10739cc812f3e510edb3cc09ed0
= <=>
2024-06-22 14:09:23 +0200

    finalea

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const argv  = process.argv.slice(2);
+const argv = process.argv.slice(2);
 
 if (argv.length === 0) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (argv.length === 1) {
   console.log('Argument found');
 } else {
