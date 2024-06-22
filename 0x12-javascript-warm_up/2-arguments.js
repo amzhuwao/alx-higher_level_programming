4598f76e2d870b900b6f3173292f962ac950fbd5
= <=>
2024-06-22 13:47:58 +0200

    semistandard fix on arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 const { argv } = require('node:process');
 console.log(argv.length);
 
-if (argv.length == 0){
-    console.log("No arguments");
-}
-else if (argv.length == 1){
-    console.log("Argument found");
-}
-else{
-    console.log("Arguments found");
+if (argv.length === 0) {
+  console.log('No arguments');
+} else if (argv.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
